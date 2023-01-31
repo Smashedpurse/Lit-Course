831da997e63ab8eea17089522211ff67ccdb2dc2
Ivan Rubio <smashedpurse@gmail.com>
2023-01-31 11:29:55 -0600

    Develope and understand how the events work in lit

--- components/eit-counter.js
+++ components/eit-counter.js
@@ -4,8 +4,8 @@ export class MyCounter extends LitElement {
   static styles = [
     css`
       :host {
-        display: block;
-        border: 1px solid red;
+        display: inline-block;
+        border: 1px solid;
       }
       h2 {
         color: red;
@@ -14,22 +14,52 @@ export class MyCounter extends LitElement {
         color: blue;
         font-size: 2rem;
       }
+      .contador{
+        color: blue;
+      }
+      @media(min-width:500px){
+        .parrafo{
+            font-size:3em;
+        }
+      }
+
+      input{
+        width:30px
+      }
     `,
   ];
 
-  static properties() {
-    return {
-      counter: { type: Number },
-    };
+  static properties={
+    counter: {
+         type: Number,
+        reflect:true }    
+  }
+
+  constructor(){
+    super();
+    this.counter=0;
   }
 
   render() {
     return html`
+        <slot></slot>
       <p>Mi <b>Contador</b>!!!!</p>
-      <p class="parrafo">!!!Otra cosa</p>
-      <h2>Soy un h2 de color rojo</h2>
+      <h3 class="contador">${this.counter}</h3>
+      <p>
+        <input id="quantity" type="text">
+      </p>
+      <button @click=${this.increment}>+ 1</button>
+      <button @click=${this.decrement}>- 1</button>
     `;
   }
+
+    increment(){
+        this.counter++;
+    }
+
+    decrement(){
+        this.counter--;
+    }
 }
 
 customElements.define("my-counter", MyCounter);
