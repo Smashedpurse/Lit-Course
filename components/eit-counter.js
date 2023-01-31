1e298c39a55b4cf1951f6e63b831b4c92a0b6e10
Ivan Rubio <smashedpurse@gmail.com>
2023-01-31 13:54:55 -0600

    Add a restart function to a new button using all the knowledges earned

--- components/eit-counter.js
+++ components/eit-counter.js
@@ -1,13 +1,14 @@
 import { LitElement, html, css } from "lit";
 import { WiredButton } from 'wired-elements/lib/wired-button.js';
 import { WiredInput } from 'wired-elements/lib/wired-input.js';
+import { WiredCard } from 'wired-elements/lib/wired-card.js';
+import { WiredSlider } from 'wired-elements/lib/wired-slider.js';
 
 export class MyCounter extends LitElement {
   static styles = [
     css`
       :host {
-        display: inline-block;
-        border: 1px solid;
+        display:inline-block;
       }
       h2 {
         color: red;
@@ -24,13 +25,24 @@ export class MyCounter extends LitElement {
             font-size:3em;
         }
       }
-      wired-button{
+      .increment{
         background-color:blue;
       }
-      input{
-        width:30px;
+      wired-input{
+        background-color:yellow;
+        width:60px;
         font-size:1.5em;
         padding:0.5em;
+      }
+      .decrement{
+        background-color:red;
+      }
+      .restart{
+        background-color:yellow;
+      }
+      wired-card{
+        margin:1em;
+        padding:1em;
       }
     `,
   ];
@@ -38,28 +50,45 @@ export class MyCounter extends LitElement {
   static properties={
     counter: {
          type: Number,
-        reflect:true }    
+        reflect:true },
+    quantity:{
+        type:Number
+    }  
   }
 
   constructor(){
     super();
     this.counter=0;
+    this.quantity=10;
   }
 
   render() {
     return html`
+    <wired-card elevation="3">
         <slot></slot>
       <p>Mi <b>Contador</b>!!!!</p>
       <h3 class="contador">${this.counter}</h3>
       <p>
-        <wired-input id="quantity" type="number" value="1">
+        <wired-input id="quantity" type="number" .value="${this.quantity}"></wired-input>
       </p>
-      
-      <wired-button elevation="3" outline @click=${this.increment}>Incrementar</wired-button>
-      <wired-button elevation="3" outline @click=${this.decrement}>Decrementar</wired-button>
-      
+      <p><wired-slider 
+      value="10" 
+      min="1" 
+      max="20" 
+      @change=${this.doChangeQuantity}>
+    </wired-slider>
+    </p>
+      <wired-button elevation="10" outline @click=${this.increment} class="increment">Incrementar</wired-button>
+      <wired-button elevation="3" outline @click=${this.decrement} class="decrement">Decrementar</wired-button>
+      <wired-button elevation="3" outline @click=${this.restart} class="restart">Reiniciar</wired-button>
+  </wired-card>
     `;
   }
+
+
+    doChangeQuantity(e){
+        this.quantity=e.detail.value;
+    }
 
     increment(){
         let quantity = this.shadowRoot.getElementById('quantity').value
@@ -69,6 +98,10 @@ export class MyCounter extends LitElement {
     decrement(){
         let quantity = this.shadowRoot.getElementById('quantity').value
         this.counter--;
+    }
+
+    restart(){
+        this.counter=0;
     }
 }
 
